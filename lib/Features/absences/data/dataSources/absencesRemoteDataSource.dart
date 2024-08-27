fdf3fdc3b40d65b810ae62d55288d4c822815c57
mohammad trablsi <hamoud9trap@gmail.com>
2024-08-28 01:18:52 +0300

    make some of changes

--- lib/Features/absences/data/dataSources/absencesRemoteDataSource.dart
+++ lib/Features/absences/data/dataSources/absencesRemoteDataSource.dart
@@ -1,15 +1,12 @@
-
 import 'package:ehsan/Features/absences/data/models/absencesModel.dart';
 import 'package:ehsan/Features/absences/domain/entites/absencesEntity.dart';
 import 'package:ehsan/core/utils/api_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-
-
-
-
 abstract class AbsencseRemoteDataSource {
-  Future<AbsencseEntity> viewAbsencse({required Map<String, dynamic> header,required Map<String, dynamic> body});
+  Future<AbsencseEntity> viewAbsencse(
+      {required Map<String, dynamic> header,
+      required Map<String, dynamic> body});
 }
 
 class AbsencseRemoteDataSourceImpl extends AbsencseRemoteDataSource {
@@ -19,16 +16,12 @@ class AbsencseRemoteDataSourceImpl extends AbsencseRemoteDataSource {
 
   @override
   Future<AbsencseEntity> viewAbsencse(
-      {required Map<String, dynamic> header,required Map<String, dynamic> body}) async {
+      {required Map<String, dynamic> header,
+      required Map<String, dynamic> body}) async {
     var response = await apiService.get(
-      headers: header,
-        data: body, endPoint: 'showStudentAbsence');
+        headers: header, data: body, endPoint: 'showStudentAbsence');
     AbsencseEntity entity;
     entity = AbsencseModel.fromJson(response);
-    // SharedPreferences prefs = await SharedPreferences.getInstance();
-    // await prefs.setInt('userId', entity.id);
-    // print("iddddddddddddddddddddddddddddddddddd${entity.id}");
     return entity;
   }
-
 }
